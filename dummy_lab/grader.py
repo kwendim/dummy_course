d6f23f2be6eb21bd729fc6ef0f7a66453516e778
Kidus Mammo <kidus.mammo@aalto.fi>
2020-06-05 20:13:18 +0300

    should pass automatically

--- dummy_lab/grader.py
+++ dummy_lab/grader.py
@@ -5,17 +5,17 @@ from onpit import Grader
 class LabGrader(Grader):
 	@Grader.addStep(name='step1')
 	def step1(self, workingDir, inputCommand):
-		pass
+	    return True
 
 	@Grader.addStep(name='step2')
 	def step2(self, workingDir, inputCommand):
-		pass
+	    return True
 
 	@Grader.addStep(name='step3')
 	def step3(self, workingDir, inputCommand):
-		pass
+	    pass
 
 	@Grader.addStep(name='step4')
 	def step4(self, workingDir, inputCommand):
-		pass
+	    pass
 
