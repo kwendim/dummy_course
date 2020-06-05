1a3f68a04e56850f879db8500016798d1df11874
Kidus Mammo <kidus.mammo@aalto.fi>
2020-06-05 20:12:16 +0300

    dummy course

---
+++ dummy_lab/grader.py
@@ -0,0 +1,21 @@
+#This is a template for the grader
+
+from onpit import Grader
+
+class LabGrader(Grader):
+	@Grader.addStep(name='step1')
+	def step1(self, workingDir, inputCommand):
+		pass
+
+	@Grader.addStep(name='step2')
+	def step2(self, workingDir, inputCommand):
+		pass
+
+	@Grader.addStep(name='step3')
+	def step3(self, workingDir, inputCommand):
+		pass
+
+	@Grader.addStep(name='step4')
+	def step4(self, workingDir, inputCommand):
+		pass
+
